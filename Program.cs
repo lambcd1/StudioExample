5bef8fb2ec35137e091c511d690554b83a3dbb89
Joseph McKee (1000087426) <MCKEJI2@student.op.ac.nz>
2023-09-12 11:26:39 +1200

    Joes new code addition

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@
             Console.WriteLine("Hello, World! Hi from Joe");
             Console.WriteLine("Hi,I am Denise");
             Console.WriteLine("Cam is here too :)");
+
+            for(int i = 0; i >100; i++)
+            {
+                Console.WriteLine("git is cool.");
+            }
+
+
         }
     }
 }
