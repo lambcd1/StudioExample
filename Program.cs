470800b2940ebed071abaa6eb881c5276ac35f98
Joseph McKee (1000087426) <MCKEJI2@student.op.ac.nz>
2023-09-12 11:33:28 +1200

    Joe added Changes

--- Program.cs
+++ Program.cs
@@ -11,9 +11,11 @@
             for(int i = 0; i < 100; i++)            /* Cam fixed operator < */
             {
                 Console.WriteLine("git is cool.");
+                Console.WriteLine("Cam is not very good at using git, why do we have to be in a team with this guy!!! -  from Denise");
             }
 
 
+            Console.WriteLine("Blah Blah Blah");
         }
     }
 }
